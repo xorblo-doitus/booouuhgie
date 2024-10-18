ea685bd9e07e8fd5ae75489d842bfcd2bb08de83
xorblo-doitus <75997617+xorblo-doitus@users.noreply.github.com>
2024-10-18 08:29:48 +0200

    Petrify

--- src/world/entities/ghost/ghost.gd
+++ src/world/entities/ghost/ghost.gd
@@ -4,8 +4,37 @@ extends CharacterBody2D
 const SPEED = 100.0
 
 
+var _falling: bool = false
+
+
+@onready var light_detector: Area2D = $LightDetector
+
+
+func push(x: float) -> void:
+	if not _falling:
+		return
+	
+	var ini := velocity
+	velocity.x = x
+	velocity.y = 0
+	move_and_slide()
+	velocity = ini
+
+
 func _physics_process(delta: float) -> void:
-	track_player(delta)
+	_falling = not light_detector.get_overlapping_areas().is_empty()
+	
+	if _falling:
+		fall(delta)
+	else:
+		track_player(delta)
+
+
+func fall(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+		
+		move_and_slide()
 
 
 func track_player(delta: float) -> void:
