7e8698c6142d5d4c7afcfcc336db71cee8d8461e
xorblo-doitus <75997617+xorblo-doitus@users.noreply.github.com>
2024-10-20 14:02:10 +0200

    Add debug level skip

--- src/world/levels/door.gd
+++ src/world/levels/door.gd
@@ -15,11 +15,22 @@ func _ready() -> void:
 	if target == null:
 		push_warning("No target for door %s in %s" % [owner.get_path_to(self), owner.scene_file_path])
 	
+	area_2d.input_pickable = OS.is_debug_build()
+	
 	area_2d.body_entered.connect(_on_player_touched)
 
 
-func _on_player_touched(_body) -> void:
+func trigger() -> void:
 	if target == null:
 		return
 	
 	travel_to.emit(target)
+
+
+func _on_player_touched(_body) -> void:
+	trigger()
+
+
+func _on_area_2d_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
+	if event is InputEventMouseButton && event.is_pressed() && event.button_index == MOUSE_BUTTON_LEFT:
+		trigger()
