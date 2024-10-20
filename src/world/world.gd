da7465bcb5d8f4bc508894fb2e272de2480d1c1e
xorblo-doitus <75997617+xorblo-doitus@users.noreply.github.com>
2024-10-20 16:54:33 +0200

    Add door SFX

--- src/world/world.gd
+++ src/world/world.gd
@@ -4,12 +4,17 @@ extends Node2D
 
 const LEVEL_1 = preload("res://src/world/levels/level_1.tscn")
 
+@onready var level_change: AudioStreamPlayer2D = $LevelChange
 
 func _ready():
 	change_level(LEVEL_1)
 
 
-func change_level(scene: PackedScene) -> void:
+func change_level(scene: PackedScene, origin: Door = null) -> void:
+	if origin:
+		level_change.global_position = origin.global_position
+		level_change.play()
+	
 	if has_node(^"CurrentLevel"):
 		$CurrentLevel.queue_free()
 		while has_node(^"CurrentLevel"):
@@ -20,4 +25,4 @@ func change_level(scene: PackedScene) -> void:
 	add_child(new_level)
 	
 	for door: Door in get_tree().get_nodes_in_group(&"door"):
-		door.travel_to.connect(change_level)
+		door.travel_to.connect(change_level.bind(door))
